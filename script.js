7a153f75a2faceaf698593f39ca9a8d0813a2df9
Tyler Vogt <tylervog@buffalo.edu>
2023-01-30 19:45:39 -0500

    Added keyboard support as well as calculations

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
+document.onkeydown = (e) => inputKeys(e);
+
 function inputKeys(button){
     var elem = document.getElementById("result-textbox");
-    elem.value += button.textContent;
+    if(button instanceof KeyboardEvent){
+        switch(button.key){
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case '0':
+            case '-':
+            case '+':
+            case '.':
+            case '*':
+            case '/':
+                elem.value += button.key;
+                break;
+            case "Backspace":
+            case "Delete":
+                elem.value = elem.value.substring(0,elem.value.length-1);
+                break;
+            case '=':
+            case "Enter":
+                calculate(elem);
+                break;
+        }
+    }else{
+        if(button.textContent === '='){
+            calculate(elem);
+        }else{
+            elem.value += button.textContent;
+        }
+        button.blur();//Removes focus from button
+    }
+}
 
+function calculate(elem){
+    elem.value = eval(elem.value);
 }
 
